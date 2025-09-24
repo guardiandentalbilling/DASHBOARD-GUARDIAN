8d64ed983549cb042ad932f75169efd296e74d3a
Guardiandental Billing LLC <226576101+guardiandentalbilling@users.noreply.github.com>
2025-09-24 13:56:54 +0500

    fix: add missing app.listen() call in src/server.js to properly start HTTP server

--- GuardianDashBaordAI/src/server.js
+++ GuardianDashBaordAI/src/server.js
@@ -1,10 +1,12 @@
 const { init } = require('./app');
 const config = require('./config/env');
 
-init().then(()=>{
+init().then((app)=>{
   const port = config.port;
-  // eslint-disable-next-line no-console
-  console.log(`API server listening on ${port}`);
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`API server listening on port ${port}`);
+  });
 }).catch(err => {
   console.error('Failed to start server', err);
   process.exit(1);
